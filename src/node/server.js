462dafe63704c26ed7c30817b631cf49af653c2a
John McLear <john@mclear.co.uk>
2013-10-20 22:32:48 +0000

    slight clean up

--- src/node/server.js
+++ src/node/server.js
@@ -47,8 +47,7 @@ async.waterfall([
     plugins = require("ep_etherpad-lite/static/js/pluginfw/plugins");
     hooks = require("ep_etherpad-lite/static/js/pluginfw/hooks");
     hooks.plugins = plugins;
-    metrics = require('measured');
-    stats = new metrics.Collection();
+    stats = require('measured').createCollection();
     stats.counter('totalUsers');
     callback();
   },
